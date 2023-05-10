36da781f13f541181b07aff7a308b8b0dbcc175c
Kauan Bomfim <111811804+wkauan@users.noreply.github.com>
2023-05-10 18:29:42 -0300

    Add banco de dados

---
+++ vestibular.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 10/05/2023 às 23:42
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `vestibular`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `candidato`
+--
+
+CREATE TABLE `candidato` (
+  `candidatoID` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `dados` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `oriundo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `candidato`
+--
+ALTER TABLE `candidato`
+  ADD PRIMARY KEY (`candidatoID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
